e84a455ac2e0ac4d25c0782b07d18e5c5802e6f6
nick <nsc319@gmail.com>
2012-11-03 21:24:18 -0400

    stub, test (2)

---
+++ Graphics/Test.h
@@ -0,0 +1,23 @@
+/*
+ * Test.h
+ *
+ *  Created on: Nov 3, 2012
+ *      Author: nick
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+namespace hw5
+{
+
+  class Test
+  {
+  public:
+    Test();
+    virtual
+    ~Test();
+  };
+
+} /* namespace hw5 */
+#endif /* TEST_H_ */
