2877d2293fc6716bb095530b8da10f2f4e3aaf4f
nick <nsc319@gmail.com>
2012-11-03 21:21:27 -0400

    stubbed project

---
+++ Test.cpp
@@ -0,0 +1,24 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: Nov 3, 2012
+ *      Author: nick
+ */
+
+#include "Test.h"
+
+namespace hw5
+{
+
+  Test::Test()
+  {
+    // TODO Auto-generated constructor stub
+
+  }
+
+  Test::~Test()
+  {
+    // TODO Auto-generated destructor stub
+  }
+
+} /* namespace hw5 */
